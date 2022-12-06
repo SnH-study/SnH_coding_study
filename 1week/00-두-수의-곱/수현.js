26435b3ab224888295faecd4ea21ec32e35b1446
Soohyeon Kim <tngusglaso@gmail.com>
2022-12-06 23:06:28 +0900

    solution: 수현 풀이 1일차

---
+++ 1week/00-두-수의-곱/수현.js
@@ -0,0 +1,7 @@
+// const solution = (num1, num2) => num1 * num2;
+
+function solution(num1, num2) {
+  let answer = 0;
+
+  return num1 * num2;
+}
---
+++ 1week/01-숫자-비교하기/수현.js
@@ -0,0 +1,11 @@
+const solution = (num1, num2) => {
+  let answer = 0;
+
+  if (num1 === num2) {
+    answer = 1;
+  } else {
+    answer = -1;
+  }
+
+  return answer;
+};
