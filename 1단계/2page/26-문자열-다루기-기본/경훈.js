968ec1d85d032f1f3991a828be662f0d57206bf3
chuhoon <khchu1220@naver.com>
2022-12-18 23:59:07 +0900

    Solve : 1단계 25~26 문제 풀이

---
+++ 1단계/2page/26-문자열-다루기-기본/경훈.js
@@ -0,0 +1,6 @@
+function solution(s) {
+  return (s.length == 4 || s.length == 6) && s == parseInt(s)
+}
+
+// https://velog.io/@blackwidow/parseInt%EC%99%80-Number%EC%9D%98-%EC%B0%A8%EC%9D%B4
+// parseInt와 Number의 차이
