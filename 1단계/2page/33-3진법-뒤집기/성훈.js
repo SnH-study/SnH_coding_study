c5fe3842e143ae26cec6050ce8a606a913f55b4e
tada-js <justfix010@gmail.com>
2022-12-25 21:45:35 +0900

    Solve : 1단계 30~37번 문제 해결

---
+++ 1단계/2page/33-3진법-뒤집기/성훈.js
@@ -0,0 +1,6 @@
+// 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+const solution = (n) => {
+  return parseInt(n.toString(3).split("").reverse().join(""), 3);
+};
+solution(45);
