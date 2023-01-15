0486a7826d1b288ad5c57a1049b7a89b0436c6bf
Kim Seong Hun <justfix010@gmail.com>
2023-01-16 06:14:07 +0900

    Solve : 해시 완수하지 못한 선수 문제 해결

--- 자료구조/해시_알고리즘/성훈/완수하지-못한-선수.js
+++ 자료구조/해시_알고리즘/성훈/완수하지-못한-선수.js
@@ -1,34 +1,29 @@
 const solution = (participant, completion) => {
   const map = new Map();
-  let max = Number.MIN_SAFE_INTEGER;
   let answer = "";
 
   for (let value of participant) {
-    if (map.has(value)) {
-      map.set(value, map.get(value) + 1);
-    } else {
-      map.set(value, +1);
-    }
+    map.set(value, map.get(value) + 1 || 1);
   }
 
   for (let value of completion) {
-    if (map.has(value)) {
+    if (map.get(value) === 1) {
+      map.delete(value);
+    } else {
       map.set(value, map.get(value) - 1);
     }
   }
 
-  for (let [key, value] of map) {
-    if (value > max) {
-      max = value;
-      answer = key;
-    }
+  for (let [key, _] of map) {
+    answer = key;
   }
-  return answer;
+
   // console.log(answer);
+  return answer;
 };
 solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
-// solution(
-//   ["marina", "josipa", "nikola", "vinko", "filipa"],
-//   ["josipa", "filipa", "marina", "nikola")]
-// );
-// solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
+solution(
+  ["marina", "josipa", "nikola", "vinko", "filipa"],
+  ["josipa", "filipa", "marina", "nikola"]
+);
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
