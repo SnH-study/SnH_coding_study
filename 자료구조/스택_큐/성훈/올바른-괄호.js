d886e92b2472c7a1bdb4f92780089808ed3516bf
Kim Seong Hun <justfix010@gmail.com>
2023-01-24 20:09:47 +0900

    Help : 스택큐 올바른 괄호 효율성 테스트 시간초과

--- 자료구조/스택_큐/성훈/올바른-괄호.js
+++ 자료구조/스택_큐/성훈/올바른-괄호.js
@@ -1,4 +1,7 @@
 const solution = (s) => {
+테스트케이스 2 실패
+효율성 테스트 1, 2 시간초과
+
   const stack = [];
   const sArr = s.split("");
 
@@ -13,5 +16,21 @@ const solution = (s) => {
   return stack.length === 0 ? true : false;
 };
 
-// 테스트케이스 2 실패
-// 효율성 테스트 1, 2 시간초과
+const solution2 = (s) => {
+  // 효율성 테스트 1, 2 시간초과
+  let answer = true;
+  const stack = [];
+
+  for (let value of s) {
+    if (value === "(") {
+      stack.push(value);
+    } else {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  return stack.length > 0 ? false : true;
+};
+
+solution("()()");
+solution("(()(");
