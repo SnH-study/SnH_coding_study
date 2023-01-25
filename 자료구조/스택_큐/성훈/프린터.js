fd5a0f75747b70c4afdb884dbb99d36116074999
Kim Seong Hun <justfix010@gmail.com>
2023-01-26 00:26:05 +0900

    Solve : 스택큐 프린터 문제 해결

---
+++ 자료구조/스택_큐/성훈/프린터.js
@@ -0,0 +1,21 @@
+const solution = (priorities, location) => {
+  let list = priorities.map((v, i) => {
+    return {
+      myReqDoc: i === location,
+      value: v,
+    };
+  });
+
+  let count = 0;
+  while (true) {
+    let currentShift = list.shift();
+    if (list.some((e) => e.value > currentShift.value)) {
+      list.push(currentShift);
+    } else {
+      count++;
+      if (currentShift.myReqDoc) return count;
+    }
+  }
+};
+solution([2, 1, 3, 2], 2); // 1
+// solution([1, 1, 9, 1, 1, 1], 0); // 5
