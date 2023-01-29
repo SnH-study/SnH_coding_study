a3c3ce9a16a9df3d340285a9574cae2a0733461e
chuhoon <khchu1220@naver.com>
2023-01-29 20:56:50 +0900

    Solve : 베스트 앨범 이해 풀이

--- 자료구조/스택_큐/희진/3-올바른_괄호.js
+++ 자료구조/스택_큐/희진/3-올바른_괄호.js
@@ -1,18 +1,19 @@
 function solution(s) {
-  let answer = true
-  let count = 0
-  let arr = s.split('')
+  let answer = true;
+  let count = 0;
+  let arr = s.split("");
 
   for (let i = 0; i < arr.length; i++) {
-    arr[i].includes("(") ? count++ : count--
+    arr[i].includes("(") ? count++ : count--;
     if (count < 0) {
       return false;
     }
   }
 
-  if (arr[0] !== '(' || arr[arr.length - 1] !== ')' || arr.length % 2 !== 0) return false
+  if (arr[0] !== "(" || arr[arr.length - 1] !== ")" || arr.length % 2 !== 0)
+    return false;
 
   return answer;
 }
 
-//효율성 2번 실패 ㅠㅠ 
+//효율성 2번 실패 ㅠㅠ
