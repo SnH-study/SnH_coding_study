ece57bed34e4386b33c87876a00dea4d8b113b6b
chuhoon <khchu1220@naver.com>
2023-02-24 21:52:01 +0900

    Solve : 모의고사 문제 풀이

---
+++ 자료구조/완전탐색/경훈/모의고사.js
@@ -0,0 +1,14 @@
+function solution(sizes) {
+  const sortArr = sizes.map((i) => i.sort((a, b) => b - a));
+
+  let maxArr = [];
+  let minArr = [];
+
+  sortArr.map((i) => maxArr.push(i[0]));
+  sortArr.map((i) => minArr.push(i[1]));
+
+  const maxSort = maxArr.sort((a, b) => b - a)[0];
+  const minSort = minArr.sort((a, b) => b - a)[0];
+
+  return maxSort * minSort;
+}
