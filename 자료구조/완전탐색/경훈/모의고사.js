0fbee534db2a9cf68c900bb8d9dd914716c05fc5
chuhoon <khchu1220@naver.com>
2023-02-27 22:07:29 +0900

    Solve : 모의고사 풀이

--- 자료구조/완전탐색/경훈/모의고사.js
+++ 자료구조/완전탐색/경훈/모의고사.js
@@ -1,14 +1,25 @@
-function solution(sizes) {
-  const sortArr = sizes.map((i) => i.sort((a, b) => b - a));
+function solution(answers) {
+  const a = [1, 2, 3, 4, 5];
+  const b = [2, 1, 2, 3, 2, 4, 2, 5];
+  const c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  let maxArr = [];
-  let minArr = [];
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
 
-  sortArr.map((i) => maxArr.push(i[0]));
-  sortArr.map((i) => minArr.push(i[1]));
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] == a[i % 5]) count1++;
+    if (answers[i] == b[i % 8]) count2++;
+    if (answers[i] == c[i % 10]) count3++;
+  }
 
-  const maxSort = maxArr.sort((a, b) => b - a)[0];
-  const minSort = minArr.sort((a, b) => b - a)[0];
+  let answer = [];
+  const arr = [count1, count2, count3];
+  const max = Math.max(...arr);
 
-  return maxSort * minSort;
+  for (let j = 0; j < 3; j++) {
+    if (max == arr[j]) answer.push(j + 1);
+  }
+
+  return answer.sort((a, b) => a - b);
 }
