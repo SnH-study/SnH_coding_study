a7ac35b39fcdaaa0d0a4fac84be498179f9b62bf
heejin <khj16355@naver.com>
2023-02-27 13:42:22 +0900

    Solve : 완전탐색 최소직사각형 문제 해결

---
+++ 자료구조/완전탐색/희진/최소직사각형.js
@@ -0,0 +1,11 @@
+function solution(sizes) {    
+    let w = []
+    let h = []
+    let arr = sizes.map((item)=> item.sort( ( a,b )=>a-b))
+        for (let i = 0 ; i < arr.length ; i++){
+            h.push(arr[i][0])
+            w.push(arr[i][1])
+    }
+    
+return Math.max(...h)*Math.max(...w)
+}
