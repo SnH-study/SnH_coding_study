7a9555ff7dd0a5b0c5db7fcb0fdba10b8ec384e1
chuhoon <khchu1220@naver.com>
2023-03-13 22:30:35 +0900

    Solve : 모음사전 풀이

--- 자료구조/완전탐색/경훈/모음사전.js
+++ 자료구조/완전탐색/경훈/모음사전.js
@@ -15,7 +15,102 @@ function solution(word) {
         break;
     }
   });
-  console.log(arr);
+
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {}
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      switch (arr[i]) {
+        case 0:
+          sum += 1;
+          break;
+        case 1:
+          sum += 781 * 1 + 1;
+          break;
+        case 2:
+          sum += 781 * 2 + 1;
+          break;
+        case 3:
+          sum += 781 * 3 + 1;
+          break;
+        case 4:
+          sum += 781 * 4 + 1;
+          break;
+      }
+    } else if (i === 1) {
+      switch (arr[i]) {
+        case 0:
+          sum += 1;
+          break;
+        case 1:
+          sum += 156 * 1 + 1;
+          break;
+        case 2:
+          sum += 156 * 2 + 1;
+          break;
+        case 3:
+          sum += 156 * 3 + 1;
+          break;
+        case 4:
+          sum += 156 * 4 + 1;
+          break;
+      }
+    } else if (i === 2) {
+      switch (arr[i]) {
+        case 0:
+          sum += 1;
+          break;
+        case 1:
+          sum += 31 * 1 + 1;
+          break;
+        case 2:
+          sum += 31 * 2 + 1;
+          break;
+        case 3:
+          sum += 31 * 3 + 1;
+          break;
+        case 4:
+          sum += 31 * 4 + 1;
+          break;
+      }
+    } else if (i === 3) {
+      switch (arr[i]) {
+        case 0:
+          sum += 1;
+          break;
+        case 1:
+          sum += 6 * 1 + 1;
+          break;
+        case 2:
+          sum += 6 * 2 + 1;
+          break;
+        case 3:
+          sum += 6 * 3 + 1;
+          break;
+        case 4:
+          sum += 6 * 4 + 1;
+          break;
+      }
+    } else if (i === 4) {
+      switch (arr[i]) {
+        case 0:
+          sum += 1;
+          break;
+        case 1:
+          sum += 1 * 1 + 1;
+          break;
+        case 2:
+          sum += 1 * 2 + 1;
+          break;
+        case 3:
+          sum += 1 * 3 + 1;
+          break;
+        case 4:
+          sum += 1 * 4 + 1;
+          break;
+      }
+    }
+  }
+
+  return sum;
 }
