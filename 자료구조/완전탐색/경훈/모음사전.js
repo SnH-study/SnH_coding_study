2935296f3b1c357d4438a80c6dc0c784392e8457
chuhoon <khchu1220@naver.com>
2023-03-16 17:42:16 +0900

    Solve : 모의사전 다른 방법 풀이

--- 자료구조/완전탐색/경훈/모음사전.js
+++ 자료구조/완전탐색/경훈/모음사전.js
@@ -115,3 +115,15 @@ function solution(word) {
 
   return sum;
 }
+
+function solution(word) {
+  let wordArr = word.split('');
+
+  let answer = wordArr.reduce(
+    (r, c, i) =>
+      r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1,
+    0
+  );
+
+  return answer;
+}
